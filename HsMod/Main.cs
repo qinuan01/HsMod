57ed1211491f7309fd28c9f6c410c95e7c7f666d
qinuan01 <142510302+qinuan01@users.noreply.github.com>
2025-07-23 10:19:15 +0800

    Update Main.cs

--- HsMod/Main.cs
+++ HsMod/Main.cs
@@ -194,21 +194,21 @@ namespace HsMod
             {
                 if (keyTimeGearUp.Value.IsDown())
                 {
-                    if (timeGear.Value == 10) return;
+                    if (timeGear.Value == 12) return;
                     if (timeGear.Value <= -2 || timeGear.Value >= 2) timeGear.Value += 1;
                     else timeGear.Value = 2;
                     return;
                 }
                 else if (keyTimeGearDown.Value.IsDown())
                 {
-                    if (timeGear.Value == -10) return;
+                    if (timeGear.Value == -12) return;
                     if (timeGear.Value <= -2 || timeGear.Value >= 2) timeGear.Value -= 1;
                     else timeGear.Value = -2;
                     return;
                 }
                 else if (keyTimeGearMax.Value.IsDown())
                 {
-                    timeGear.Value = timeGear.Value >= 4 ? 10 : 4;
+                    timeGear.Value = timeGear.Value >= 4 ? 12 : 4;
                     return;
                 }
                 else if (keyTimeGearDefault.Value.IsDown())
