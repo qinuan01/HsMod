e9cd226bc644fe034a2cd05d0163728e8f9e19ee
qinuan01 <142510302+qinuan01@users.noreply.github.com>
2025-08-14 09:53:09 +0800

    Update Main.cs

--- HsMod/Main.cs
+++ HsMod/Main.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using static HsMod.PluginConfig;
 
-
+//
 namespace HsMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
