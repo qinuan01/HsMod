6859502198afce0f740ba7e1aeb18ccb43fc9c7e
qinuan01 <142510302+qinuan01@users.noreply.github.com>
2025-08-26 20:41:01 +0800

    Update Main.cs

--- HsMod/Main.cs
+++ HsMod/Main.cs
@@ -208,7 +208,7 @@ namespace HsMod
                 }
                 else if (keyTimeGearMax.Value.IsDown())
                 {
-                    timeGear.Value = timeGear.Value >= 4 ? 64 : 23;
+                    timeGear.Value = timeGear.Value >= 4 ? 100 : 23;
                     return;
                 }
                 else if (keyTimeGearDefault.Value.IsDown())
